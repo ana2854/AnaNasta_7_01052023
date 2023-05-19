1800f0e259f90968c3e9c62a0bce72313a6c5e20
Ana Nasta <univpro81@gmail.com>
2023-05-19 18:17:35 +0200

    backend connexion à la bdd sql avec package mysql2 OK

--- backend/app.js
+++ backend/app.js
@@ -6,4 +6,37 @@ app.use((req, res) => {
    res.json({ message: 'Votre requête a bien été reçue !' }); 
 });
 
+
+//importation de sql
+const mysql2 = require("mysql2")
+
+//connection à la database mysql
+
+
+// MYSQL
+const connectionToMySql = mysql2.createConnection({
+    host: "localhost",
+    user: "groupomania_admin",
+    password: "yxAi9mSgAD4O",
+    database: "groupomania",
+  })
+  
+  function connectionToMySqlDatabase() {
+    connectionToMySql.connect((err) => {
+      if (err) {
+        console.error("Non connecté à la bdd mysql: ", err)
+        return
+      }
+      console.log("Connecté à la bdd mysql")
+    })
+  }
+  
+  connectionToMySqlDatabase()
+  
+  
+
+
+
+
+
 module.exports = app;
