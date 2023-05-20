8a1fa85d256b7c07e76aba509fb1cb1d0da4c07b
Ana Nasta <univpro81@gmail.com>
2023-05-20 15:43:18 +0200

    connection sequelize db réussie-sans préciser package sql2

--- backend/app.js
+++ backend/app.js
@@ -1,69 +1,38 @@
 //création app express
-const express = require('express');
+const express = require("express")
 
-const app = express();
+const app = express()
 
 app.use((req, res) => {
-   res.json({ message: 'Votre requête a bien été reçue !' }); 
-});
+  res.json({ message: "Votre requête a bien été reçue !" })
+})
 
 //dotenv
-require('dotenv').config()
+require("dotenv").config()
 
 //importation sequelize
 const { Sequelize } = require("sequelize")
 
-//importation de sql
-const mysql2 = require("mysql2")
-
-//connection à la database mysql
-
-
-// MYSQL
-/*
-const connectionToMySql = mysql2.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  })
-  
-  function connectionToMySqlDatabase() {
-    connectionToMySql.connect((err) => {
-      if (err) {
-        console.error("Non connecté à la bdd mysql: ", err)
-        return
-      }
-      console.log("Connecté à la bdd mysql")
-    })
-  }
-
-  
-  connectionToMySqlDatabase()
-  */
-  //SEQUELIZE connection BDD
-
-  //SEQUELIZE
+//SEQUELIZE
 const sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USERNAME,
-    process.env.DB_PASSWORD,
-    {
-      host: process.env.DB_HOST,
-      dialect: "mysql",
-    }
-  )
-  
-  async function connectionToSequelizeDb() {
-    try {
-      await sequelize.authenticate()
-      console.log("Connecté à la bdd Sequelize")
-    } catch (error) {
-      console.error("Non connecté à la bdd Sequelize", error)
-    }
+  process.env.DB_NAME,
+  process.env.DB_USERNAME,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: "mysql",
   }
-  
-  connectionToSequelizeDb()
+)
 
+async function connectionToSequelizeDb() {
+  try {
+    await sequelize.authenticate()
+    console.log("Connecté à la bdd Sequelize")
+  } catch (error) {
+    console.error("Non connecté à la bdd Sequelize", error)
+  }
+}
 
-module.exports = app;
+connectionToSequelizeDb()
+
+module.exports = app
