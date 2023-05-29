2ab23a5bdd171d119e51c99c071a108fd931b075
Ana Nasta <univpro81@gmail.com>
2023-05-29 21:06:27 +0200

    style page login brouillon

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -5,7 +5,7 @@ import { Login } from "./Login"
 function App() {
   return (
     <div>
-      <h1>Se connecter</h1>
+      
       <Login />
     </div>
   )
--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,23 +1,37 @@
 export function Login() {
   return (
     <form>
-      <label htmlFor="email">Email:</label>
-      <input type="email" id="email" className="email" required placeholder="email" aria-required="true" autoFocus
-      />
-      <p>Veuillez saisir une adresse email valide</p>
-      <br />
-      <label htmlFor="password">Mot de passe :</label>
-      <input
-        type="password" id="password" className="password"
-        required placeholder="Mot de passe" aria-required="true"
-        minlength={4} maxLength={50}
-      />
-      <p>
-        
-        Veuillez indiquer 4 caractères minimum dont au moins une lettre minuscule, une lettre majuscule, un chiffre et un caractère spéciale
-      </p>
-      <br />
-      <button type="submit">Se connecter</button>
+      <h1>Se connecter</h1>
+      <div className="form-container">
+        <label htmlFor="email">Email:</label>
+        <input
+          type="email"
+          id="email"
+          className="email"
+          required
+          placeholder="Votre email"
+          aria-required="true"
+          autoFocus
+        />
+        <p className="error-email">Veuillez saisir une adresse email valide</p>
+
+        <label htmlFor="password">Mot de passe :</label>
+        <input
+          type="password"
+          id="password"
+          className="password"
+          required
+          placeholder="Mot de passe"
+          aria-required="true"
+          minlength={4}
+          maxLength={50}
+        />
+        <p className="error-password">
+          Veuillez indiquer 4 caractères minimum dont au moins une lettre
+          minuscule, une lettre majuscule, un chiffre et un caractère spéciale
+        </p>
+      </div>
+      <button type="submit">Connexion</button>
       <p>Vous n'avez pas encore de compte ? </p>
       <a href="">Créer un compte</a>
     </form>
