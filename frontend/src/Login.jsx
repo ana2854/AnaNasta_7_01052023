1e46e1b867fbd140570de9fc750386cec1ea4f71
Ana Nasta <univpro81@gmail.com>
2023-06-01 16:22:01 +0200

    brouillon react login

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,37 +1,47 @@
 import React from 'react'
+import { useState } from 'react'
 import logo from '../src/image/groupomania-logoBW.svg'
 import './styles.css'
 
 export function Login() {
+
+const [email, setEmail] = useState("")
+const [password, setPassword] = useState("")
+
+
+
   return (
-    <form>
+    <form method="post">
+    
       <img src={logo} className="logo-groupomania" ></img>
       
       <div className="form-container">
         <label htmlFor="email">Email</label>
-        <input
+        <input 
           type="email"
           id="email"
-          className="email"
           required
           placeholder="Votre email"
           aria-required="true"
           autoFocus
+          value={email}
+          onChange={e => setEmail(e.target.value)}
         />
-        <p className="error-email">Veuillez saisir une adresse email valide</p>
+        <p className="error-email" aria-hidden="true">Veuillez saisir une adresse email valide</p>
 
         <label htmlFor="password">Mot de passe</label>
         <input
           type="password"
           id="password"
-          className="password"
           required
           placeholder="Mot de passe"
           aria-required="true"
-          minlength={4}
+          minLength={4}
           maxLength={50}
+          value={password}
+          onChange={e => setPassword(e.target.value)}
         />
-        <p className="error-password">
+        <p className="error-password" aria-hidden="true">
           Veuillez indiquer 4 caractères minimum dont au moins une lettre
           minuscule, une lettre majuscule, un chiffre et un caractère spéciale
         </p>
