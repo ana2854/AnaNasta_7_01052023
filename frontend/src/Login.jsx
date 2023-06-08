1dcad865d805cdc1add549b9bcd5e561d124f18d
Ana Nasta <univpro81@gmail.com>
2023-06-08 15:36:33 +0200

    qqs modifications page login

--- frontend/src/Login.jsx
+++ frontend/src/Login.jsx
@@ -1,54 +1,70 @@
-import React from 'react'
-import { useState } from 'react'
-import logo from '../src/image/groupomania-logoBW.svg'
-import './styles.css'
+import React from "react"
+import { useState, useEffect } from "react"
+import logo from "../src/image/groupomania-logoBW.svg"
+import "./styles.css"
 
 export function Login() {
+  //variables email&password qui vont changer d'état
+  const [email, setEmail] = useState("")
+  const [password, setPassword] = useState("")
 
-const [email, setEmail] = useState("")
-const [password, setPassword] = useState("")
+  //fonction onSubmit de react pour gérer l'envoi des datas
+  function onSubmit(e) {
+    e.preventDefault()
+  }
 
-
+  useEffect(()=> {
+    console.log("render")
+  })
 
   return (
-    <form method="post">
-    
-      <img src={logo} className="logo-groupomania" ></img>
-      
-      <div className="form-container">
-        <label htmlFor="email">Email</label>
-        <input 
+    <form onSubmit={onSubmit} class="form">
+      <img src={logo} className="logo-groupomania"></img>
+
+ 
+      <div className="form-container error">
+
+        <label class="label" htmlFor="email">Email</label>
+        <input
           type="email"
           id="email"
+          class="input"
           required
           placeholder="Votre email"
           aria-required="true"
           autoFocus
           value={email}
-          onChange={e => setEmail(e.target.value)}
-        />
-        <p className="error-email" aria-hidden="true">Veuillez saisir une adresse email valide</p>
-
-        <label htmlFor="password">Mot de passe</label>
+          onChange={(e) => setEmail(e.target.value)}/>
+          
+        <div class="error-message">erreur email </div>
+          
+    
+      
+        <label className="label" htmlFor="password">Mot de passe</label>
         <input
           type="password"
           id="password"
+          class="input"
           required
           placeholder="Mot de passe"
           aria-required="true"
           minLength={4}
           maxLength={50}
           value={password}
-          onChange={e => setPassword(e.target.value)}
+          onChange={(e) => setPassword(e.target.value)}
         />
-        <p className="error-password" aria-hidden="true">
-          Veuillez indiquer 4 caractères minimum dont au moins une lettre
-          minuscule, une lettre majuscule, un chiffre et un caractère spéciale
-        </p>
+
+        <div class="error-message">erreur mot de passe</div>
+        
       </div>
-      <button type="submit">Connexion</button>
+
+      <button class="btn" type="submit">Connexion</button>
+
       <p>Vous n'avez pas encore de compte ? </p>
-      <a href="">Créer un compte</a>
+      <a href="">Créer un compte !</a>
+
+      
+      
     </form>
   )
 }
