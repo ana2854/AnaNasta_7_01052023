89b170bfa65aa3976a888c86acf2bf67572e832a
Ana Nasta <univpro81@gmail.com>
2023-06-18 15:50:44 +0200

    changement router

--- frontend/src/pages/Post.jsx
+++ frontend/src/pages/Post.jsx
@@ -2,12 +2,20 @@ export function Post () {
 
 return ( 
 <>
-<h1>A post</h1>
-    <span>By : dynamic name</span>
+
+    <span className="post-username">By : dynamic name</span>
     {/*By: <Link to={`/users/${user.id}`}>{user.name}</Link>)*/}
 
-<div >POST BODY HERE</div>
+
+<div className="post-content" >POST BODY HERE</div>
 {/*post.body */}
+
+<div className="post-date">Crée le: dynamic date</div>
+
+<button className="btn-post-like">POUCE BLEU</button>
+<button className="btn-post-modify">MODIFIER</button>
+<button className="btn-post-delete">SUPPRIMER</button>
+
 </>
 
 )
--- frontend/src/router.jsx
+++ frontend/src/router.jsx
@@ -1,36 +1,30 @@
-import { createBrowserRouter } from "react-router-dom"
-import { NavLayout } from "./Nav/NavLayout"
+import { createBrowserRouter, Navigate } from "react-router-dom"
 
-import { Login } from './pages/Login';
-import { Register } from './pages/Register';
-import { PostList } from "./pages/PostList"
-import PrivateRoutes from "./utils/PrivateRoutes";
+//import {Routes, route } from "react-router-dom"
+//import { NavLayout } from "./Nav/NavLayout"
+
+import { Login } from "./pages/Login"
+import { Register } from "./pages/Register"
+//import { PostList } from "./pages/PostList"
+//import PrivateRoutes from "./utils/PrivateRoutes";
 //import { Post } from './pages/Post';
 // import { UserAccount } from './pages/UserAccount';
 // import { UserProfil } from './pages/UserProfil';
 
 export const router = createBrowserRouter([
-  
-    {
-      path: "/",
-      children: [
-        {
-        
-          children: [
-            { path: "login", element: <Login /> },
-            { path: "register", element: <Register /> },
-           
-            {
-              path: "posts:userId",
-              element: <NavLayout />, 
-              children: [
-                { path: "posts:userId", element: <PrivateRoutes component={PostList} /> },
-              ],
-            },
-          ],
-        },
-      ],
-    },
-  
-])
 
+  {
+    path: "/",
+    element: <Navigate to="/login" replace={true} />, // Redirect to "/login" when the root URL is accessed
+  },
+
+
+  {
+    path: "/login",
+    element: <Login />,
+  },
+  {
+    path: "/register",
+    element: <Register />,
+  },
+])
